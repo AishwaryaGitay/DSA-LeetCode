07e205be7eb82c78877a4e7f7a3a968f72b4eca4
Aishwarya Gitay <167085820+AishwaryaGitay@users.noreply.github.com>
2025-04-15 11:46:10 +0100

    Time: 1 ms (99.46%), Space: 45.9 MB (65.35%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+       int insert = 0;
+				
+	   for(int num : nums) {
+			if(num != 0) {
+				nums[insert] = num;
+				insert++;
+			}
+		}
+				
+		while(insert < nums.length) {
+			nums[insert] = 0;
+			insert++;
+		} 
+    }
+}
