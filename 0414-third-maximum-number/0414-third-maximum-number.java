c39fd71649aaf7ebcf83dce4d2ce62083c84eaa0
Aishwarya Gitay <167085820+AishwaryaGitay@users.noreply.github.com>
2025-10-13 14:33:44 +0100

    Time: 3 ms (74.01%), Space: 43.6 MB (37.98%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,20 +1,53 @@
 class Solution {
     public int thirdMax(int[] nums) {
         
-       TreeSet<Integer> ts = new TreeSet<Integer>();
-    	 for (int num : nums) {
-    		 ts.add(num);
-    		 if(ts.size() > 3) {
-    			 ts.pollFirst();
-    		 }
-    	 }
-    	 
-    	 if(ts.size() < 3) {
-    		 while(ts.size() > 1) {
-    			 ts.pollFirst();
-    		 }
-    	 }
-    	 
-    	 return ts.pollFirst(); 
+       Long first = null;
+        Long second = null;
+        Long third = null;
+
+        for (int num : nums) {
+            long n = (long) num;
+
+            if ((first != null && n == first) ||
+                    (second != null && n == second) ||
+                    (third != null && n == third)) {
+                continue;
+            }
+
+            if(first == null || n > first){
+                third = second;
+                second = first;
+                first = n;
+            } else if(second == null || n > second){
+                third = second;
+                second = n;
+            } else if(third == null || n > third){
+                third = n;
+            }
+        }
+
+        if (third == null) return first.intValue();
+        return third.intValue();
     }
+
+/*
+    public int thirdMax(int[] nums) {
+        // TreeSet with reverse order (largest first)
+        TreeSet<Integer> top3 = new TreeSet<>(Collections.reverseOrder());
+        
+        // Add all elements (duplicates automatically removed)
+        for (int num : nums) {
+            top3.add(num);
+            
+            // Keep only top 3
+            if (top3.size() > 3) {
+                top3.pollLast(); // Remove smallest of top 3
+            }
+        }
+        
+        // If we have 3 elements, return the last (3rd max)
+        // Otherwise, return first (1st max)
+        return top3.size() == 3 ? top3.last() : top3.first();
+    }
+    */
 }
